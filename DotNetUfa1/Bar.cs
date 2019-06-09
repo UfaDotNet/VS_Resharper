5856fab354db05e45c291e0042d556f0b672ee73
????? ?????? (Safin_RF) <Safin_RF@directum.ru>
2019-06-09 22:35:02 +0500

    Small code example improvement

--- DotNetUfa1/Bar.cs
+++ DotNetUfa1/Bar.cs
@@ -1,19 +1,16 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
 
 namespace DotNetUfa1
 {
-    public class Bar : INotifyPropertyChanged
+    public class Bar /*: INotifyPropertyChanged, IDisposable*/
     {
         private int i3;
         private int i1;
         private int i2;
-        
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        private IDisposable f;
     }
 }
